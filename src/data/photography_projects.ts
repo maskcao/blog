4b82bcc0033af58801ad9945b105f7fb5a5d54d5
maskcao <57297061+maskcao@users.noreply.github.com>
2026-01-04 14:46:08 +0800

    Update photography_projects.ts

--- src/data/photography_projects.ts
+++ src/data/photography_projects.ts
@@ -27,59 +27,5 @@ export const PHOTOGRAPHY_PROJECTS: Project[] = [
       concept: "",
       roleDetail: ""
     }
-  },
-  {
-    id: 'p2',
-    common: {
-      category: 'Photography',
-      image: 'https://mmbiz.qpic.cn/mmbiz_jpg/MpZIibj0bjSym38IftGicAWNwhwavzH9hRcnbw6AdBMu1QHAdcibpxTfz5MhDESboc1Ny5NvXC90KTstEW33jDXTw/640?wx_fmt=jpeg&from=appmsg&tp=webp&wxfrom=5&wx_lazy=1#imgIndex=37',
-    },
-    zh: {
-      title: '人文纪实',
-      subtitle: '',
-      description: '回忆与瞬间',
-      role: '',
-      tags: [],
-      awards: [],
-      concept: "",
-      roleDetail: ""
-    },
-    en: {
-      title: 'Documentary',
-      subtitle: '',
-      description: 'Memories and moments',
-      role: '',
-      tags: [],
-      awards: [],
-      concept: "",
-      roleDetail: ""
-    },
-  },
-  {
-    id: 'p3',
-    common: {
-      category: 'Photography',
-      image: 'https://mmbiz.qpic.cn/mmbiz_jpg/MpZIibj0bjSym38IftGicAWNwhwavzH9hRsYUjlBeYKR2Vib1fUpYS1icJaftdGyuPbg70E1WxswADYA4LtLdCgicNA/640?wx_fmt=jpeg&from=appmsg&tp=webp&wxfrom=5&wx_lazy=1#imgIndex=52',
-    },
-    zh: {
-      title: '空间',
-      subtitle: '',
-      description: '那些一直塑造着我们的行为与感受的',
-      role: '',
-      tags: [],
-      awards: [],
-      concept: "",
-      roleDetail: ""
-    },
-    en: {
-      title: 'Space',
-      subtitle: '',
-      description: 'Those that have always shaped us',
-      role: '',
-      tags: [],
-      awards: [],
-      concept: "",
-      roleDetail: ""
-    }
   }
 ];
